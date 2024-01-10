ad463b101bcab79fd31adcc11de3f8bd251fedb6
Ðåvï åvêlïñð <98717652+avellin0@users.noreply.github.com>
2024-01-10 19:18:32 -0300

    Update Server.js

--- Routes/Server.js
+++ Routes/Server.js
@@ -1,11 +1,8 @@
 const express = require('express')
-
 const app = express()
 
 app.use(express.static('../Style'));
-
-
 app.listen('3000')
 
 app.route('/home').get((req, res) => res.sendFile(__dirname + '/index.html'))
-
+app.route('/supporte').post((req,res) => res.send('hello world'))
